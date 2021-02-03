09b01ae52c1d314ced4d2e727be6bdae402328bd
Sunit Raut <76087855+SunitRaut@users.noreply.github.com>
2021-02-03 12:17:51 +0530

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,8 +1,11 @@
 
-
 import pyMultiSerial as p
 
-
+# Create object of class pyMultiSerial
 gateway = p.MultiSerial()  
-gateway.start()
+#Set gateway on autopilot mode (Optional). 
+#In this mode, the gateway runs on its own and handles the complete execution flow of program. 
+#In this mode, you can't add your own code. You can register callbacks.
+gateway.autopilot()
+
 
