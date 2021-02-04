252d821c403cffeaa6f1ecd6095a851b5a056c07
Sunit Raut <76087855+SunitRaut@users.noreply.github.com>
2021-02-04 23:21:10 +0530

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,8 +1,11 @@
+import time
 
 import pyMultiSerial as p
 
 # Create object of class pyMultiSerial
 gateway = p.MultiSerial()
+
+gateway.baudrate = 9600
 
 #Add Callbacks
 #Callback functions provide you an interface to perform an action at certain event.
@@ -14,20 +17,28 @@ List of Events for callback:
 4. Callback to main thread to add other miscellaneous programming
 '''  
 # Callback function on detecting a port connection.
-# Parameters: Port Number
+# Parameters: Port Number, Serial Port Object
 # Return: True if the port is to be accepted, false if the port is to be rejected based on some condition
-def port_connection_found_callback(portno):
+def port_connection_found_callback(portno, serial):
     print ("Port Found: "+portno)
-    return True
-    pass
+    time.sleep(1.5)
+    serial.write(b"Hello\n")
+    y = serial.readline()
+    print(y)
+    if y==b"Hello\n":
+        print("Port Accepted")
+        return True
+    else:
+        print("Port rejected")
+        return False
 
 #register callback function
 gateway.port_connection_found_callback = port_connection_found_callback
 
 
 # Callback on receiving port data
-# Parameters: Port Number, Text read from port
-def port_read_callback(portno, text):
+# Parameters: Port Number, Serial Port Object, Text read from port
+def port_read_callback(portno, serial, text):
     print ("Received '"+text+"' from port "+portno)
     pass
 
@@ -44,6 +55,7 @@ def port_disconnection_callback(portno):
 gateway.port_disconnection_callback = port_disconnection_callback
 
 
+
 #Set gateway on autopilot mode (Optional). 
 #In this mode, the gateway runs on its own and handles the complete execution flow of program. 
 #In this mode, you can't add your own code. You can register callbacks.
@@ -54,3 +66,8 @@ gateway.autopilot()
 # Do not write any code below gateway.autopilot(), as this method takes over the main thread.
 # Rely on callbacks to add your own code / funcionality into this program.
     
+
+
+
+
+
