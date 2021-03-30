08060ef12c483d6ab974c0335a5068bff254bd5f
Sunit Raut <76087855+SunitRaut@users.noreply.github.com>
2021-03-30 23:54:31 +0530

    Update pyMultiSerial.py

--- src/pyMultiSerial/pyMultiSerial.py
+++ src/pyMultiSerial/pyMultiSerial.py
@@ -48,8 +48,11 @@ class MultiSerial():
     close = False
     ports = set()
     ser = [] 
+    pause_ser = []
     baudrate = 9600
     timeout = 2
+    portno_range = 29
+    monitoring_freq = 0
     port_connection_found_callback = dummy_func3
     port_read_callback = dummy_func4
     port_disconnection_callback = dummy_func2
@@ -66,8 +69,6 @@ class MultiSerial():
         try: 
             while (1):
                 '''Task Schedular goes here'''
-
-                '''   '''
                 pass
     
                 
@@ -81,15 +82,35 @@ class MultiSerial():
             self.close = True
             pass
       
+# Function to ignore future communication with port until the port is disconnected
+    def ignore_port(self,i_serial):
+        #self.ser[index].close()
+        if i_serial in self.ser:
+            self.ser.remove(i_serial)
+        pass
     
-        
+# Function to pause the reading of a serial port 
+    def pause_port(self,i_serial):
+        if i_serial not in self.pause_ser:
+            self.pause_ser.append(i_serial)
+        pass
+
+# Function to resume the reading of a serial port
+    def resume_port(self,i_serial):
+        if i_serial in self.pause_ser:
+            self.pause_ser.remove(i_serial)
+        pass
+    
+# Constructor        
     def __init__(self):
         #t1 = threading.Timer(1,self.test)
         #t1.daemon = True
         #t1.start()   
         self.scan_ports()
         pass
-
+    
+    
+# Monitor port for incoming data
     def read_sink(self,i_serial,i_port):
         
         if self.close:
@@ -97,35 +118,47 @@ class MultiSerial():
         
         
         try: 
-            if (i_serial.inWaiting()>0):
-                text = i_serial.readline().decode("utf=8")
-                #Callback
-                self.port_read_callback(i_port,i_serial,text)
-                #Callback
+            if (i_serial.inWaiting()>0) and (i_serial in self.ser):
+                if i_serial not in self.pause_ser:
+                    text = i_serial.readline().decode("utf=8")
+                    #Callback
+                    self.port_read_callback(i_port,i_serial,text)
+                    #Callback
+            elif (i_serial not in self.ser):
+                #print("Ignored")
+                text = i_serial.inWaiting()
+                #text = i_serial.readline().decode("utf=8")
             if self.close==False:
-                t = threading.Thread(target=self.read_sink,args=([i_serial,i_port]))
-                t.daemon = True
-                t.start()
+                if i_serial in self.ser:
+                    #t = threading.Thread(target=self.read_sink,args=([i_serial,i_port]))
+                    t = threading.Timer(self.monitoring_freq,self.read_sink,args=([i_serial,i_port]))
+                    t.daemon = True
+                    t.start()
+                else:
+                    #t1 = threading.Thread(target=self.read_sink,args=([i_serial,i_port,2]))
+                    t1 = threading.Timer(self.monitoring_freq,self.read_sink,args=([i_serial,i_port]))
+                    t1.daemon = True
+                    t1.start()    
         
         except serial.SerialException:
-    
+            #Callback
+            self.port_disconnection_callback(i_port)
+            #Callback
             #i_serial.close()
             if i_serial in self.ser:
-                #Callback
-                self.port_disconnection_callback(i_port)
-                #Callback
                 self.ser.remove(i_serial)
                 #print("Sink disconnected")
-                self.ports.discard(i_port)
+            self.ports.discard(i_port)
                 
+
 	
-    
+# Scan for new connections        
     def scan_ports(self):
         if self.close:
             #print("Stop scan_ports")
             return
         #print("scan_ports")
-        for i in range(29):
+        for i in range(self.portno_range):
 	# Windows Ports
             t2 = threading.Thread(target=self.port_connect,args=([i,"COM"]))
             t2.daemon = True
@@ -144,7 +177,7 @@ class MultiSerial():
         t1.start()
         
         
-        
+# Try connecting to a port        
     def port_connect(self,i,prefix):
         if self.close:
             return
@@ -155,20 +188,25 @@ class MultiSerial():
                 return
             self.ser.append(serial.Serial(port=portno,baudrate = self.baudrate, timeout=self.timeout))
             temp_index=len(self.ser)-1
-            flag = True
-            #time.sleep(1.5)
+            
+            self.ports.add(portno)
+            
+            #if self.ser in self.ser:
+            t3=threading.Thread(target=self.read_sink,args=([self.ser[temp_index],portno]))
+            t3.daemon=True
+            t3.start()
+        
             #Callback
-            flag = self.port_connection_found_callback(portno, self.ser[temp_index])
+            self.port_connection_found_callback(portno, self.ser[temp_index])
             #Callback
 
-            if flag==True:
-                self.ports.add(portno)
-                t3=threading.Thread(target=self.read_sink,args=([self.ser[temp_index],portno]))
-                t3.daemon=True
-                t3.start()
-            else:
-                self.ser[temp_index].close()
-                self.ser.pop(temp_index)
+
+            
+
+
+            
+            
+            
         except IOError:
             pass
         
