505289cecd607da57cb6b53bd08c4254f1987b2b
Sunit Raut <76087855+SunitRaut@users.noreply.github.com>
2022-10-07 22:47:05 +0530

    Update Arduino.ino

--- Examples/Python - Arduino  Communication Examples/Arduino.ino
+++ Examples/Python - Arduino  Communication Examples/Arduino.ino
@@ -1,1 +1,14 @@
+// Modifyy this no so that each Arduino can be identified uniquely
+#define Arduino_no  1
 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  //print "Hello from Arduino No. 1" on serial output every 5 seconds
+  Serial.println("Hello from Arduino No."+ Arduino_no);
+}
